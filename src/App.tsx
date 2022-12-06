6f6b8c42abd778ec475748e897ddd99a96173669
Vlad Bugaets <vladbugaets@gmail.com>
2022-12-07 00:58:25 +0200

    initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+const AppWrapper = styled.div`
+  background-color: red;
+`
+function App() {
+  return (<AppWrapper>HELLO WORLD</AppWrapper>)
+}
+
+export default App
